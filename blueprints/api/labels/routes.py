3e228a6a8f8231bd95420e8db1ef3e6742b3f017
Livio <livionortes@gmail.com>
2023-01-17 11:01:04 +0100

    [Func] Ajout routes et fonctions Label

--- blueprints/api/labels/routes.py
+++ blueprints/api/labels/routes.py
@@ -1,14 +1,41 @@
-from flask import Blueprint, request
+from flask import Blueprint, jsonify
+import functions.fonctionLabels as fonctionLabels
 import app
 
 label = Blueprint('labels', __name__, url_prefix='/labels')
 
-@label.route('/getLabels', methods=['GET'])
-def getLabelsData():
-    return {'key': 'value'}
 
-@label.route('/addLabel/<name>', methods=['POST'])
-def addLabel(label):
-    print(label)
-    data = request.json
-    return {'key': 'value'}
+@label.route('/supprimerLabel/<name>', methods=['GET'])
+def supprimerLabel(name):
+    # Si la suppression de l'étiquette a réussi
+    if fonctionLabels.supprLabel(name):
+        return jsonify(success=True), 200
+    else:
+        return jsonify({
+            "status": 401,
+            "reason": "Impossible de supprimer l'étiquette"
+        }), 401
+
+
+@label.route('/getLabel/<name>', methods=['GET'])
+def getLabelData(name):
+    # Si l'obtention de l'étiquette a réussi
+    if fonctionLabels.searchLabel(name):
+        return jsonify(success=True), 200
+    else:
+        return jsonify({
+            "status": 401,
+            "reason": "Impossible de récupérer l'étiquette"
+        }), 401
+
+
+@label.route('/addLabel/<name>/<hexa>', methods=['POST', 'GET'])
+def addLabel(name, hexa):
+    # Si l'ajout de l'étiquette a réussi
+    if fonctionLabels.addLabel(name, hexa):
+        return jsonify(success=True), 200
+    else:
+        return jsonify({
+            "status": 401,
+            "reason": "Impossible d'ajouter l'étiquette"
+        }), 401
