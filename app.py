ea2cae3e4540e9e9c465538be9af52137a677677
donov <donovann.dev@gmail.com>
2023-01-17 22:32:58 +0100

    [Func] Ajout CORS (avec module flask_cors)

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 import sqlite3
 from blueprints.api.routes import api
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 app.secret_key = "4f75177439a4bb0b5494d3ce5fd4727fe7b0d61d17ffd81d6420e40c9dc7d9da"
 
 app.register_blueprint(api)
