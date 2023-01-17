95a004d6b0ecfd0c8afe68d5194944a8a31a60d0
Hugo <hgvaillant@gmail.com>
2023-01-17 11:31:21 +0100

    [Func] Routes et fonctions Users

--- blueprints/api/users/routes.py
+++ blueprints/api/users/routes.py
@@ -1,16 +1,58 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 import app
+import functions.users as functionUsers
 
 users = Blueprint('users', __name__, url_prefix='/users')
 
+
+# Route qui récupère tous les utilisateurs
 @users.route('/getUsers', methods=['GET'])
 def getUsers():
-    return []
+    return functionUsers.getUsers()
 
+
+# Route qui récupère un utilisateur selon son id
 @users.route('/getUser/<id>', methods=['GET'])
-def getUser(userID):
-    return []
+def getUser(id):
+    if not (id):
+        return jsonify({
+            "status": 400,
+            "reason": "Id Invalid"
+        }), 400
+    data = functionUsers.getUser(id)
 
+    # Si on a une taille different de 1 c'est que l'on a un autre utilisateur
+    if len(data) != 1:
+        return jsonify({
+            "status": 400,
+            "reason": "Id Invalid"
+        }), 400
+    return data
+
+
+# Route qui ajoute un utilisateur
 @users.route('/addUser', methods=['POST'])
 def addUser():
-    return []
+    # Je suis parti du principe que data est de cette forme
+    # {"nom" : "Bernard","prenom" : "Rapoe", "password" : "AZERTYUIOP"}
+    data = request.json
+
+    # Vérifie si on a nos données
+    try:
+        var = data["nom"]
+        var = data["prenom"]
+        var = data["password"]
+    except KeyError:
+        return jsonify({
+            "status": 400,
+            "reason": "First Name, Surname or Password Incomplete"
+        }), 400
+
+    # La fonction renvoie True si elle a ajouté dans la table et False sinon
+    if functionUsers.addUser(data["nom"], data["prenom"], data["password"]):
+        return jsonify(success=True), 200
+    else:
+        return jsonify({
+            "status": 400,
+            "reason": "Insertion impossible dans la base de donnée"
+        }), 400
--- functions/users.py
+++ functions/users.py
@@ -1,2 +1,57 @@
+import sqlite3
 
 
+# Fonction qui renvoie un tableau contenant tous les utilisateurs du site
+def getUsers():
+    # Connection à la table
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+
+    # Selection de tous les utilisateurs
+    res = cur.execute("SELECT * FROM users")
+    res = res.fetchall()
+
+    # Fermeture de la connection
+    cur.close()
+    con.close()
+    return res
+
+
+# Fonction qui renvoie un tableau contenant un utilisateur du site selon son id
+def getUser(userId):
+    # Connection à la table
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+
+    # Selection de l'utilisateur selon son id
+    res = cur.execute("SELECT * FROM users WHERE id=?", (userId,))
+    res = res.fetchall()
+
+    # Fermeture de la connection
+    cur.close()
+    con.close()
+    return res
+
+
+# Fonction qui permet d'ajouter un nouvel utilisateur sur le site
+def addUser(nom, prenom, password):
+    try:
+        # Connection à la table
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
+
+        # Insertion des données dans la table
+        sql = """INSERT INTO users
+                (nom, prenom, password) 
+                VALUES (?, ?, ?);"""
+        data = (nom, prenom, password)
+        cur.execute(sql, data)
+        con.commit()
+
+        # Fermeture de la connection
+        cur.close()
+        con.close()
+        return True
+    except sqlite3.Error as error:
+        print("Échec de l'insertion de la variable Python dans la table sqlite", error)
+        return False
