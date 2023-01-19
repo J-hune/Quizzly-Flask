bb689893168eb91ce1d121990104ede454859237
donov <donovann.dev@gmail.com>
2023-01-19 15:05:18 +0100

    [Fix] Correction dic envoyé par getQuestions

--- functions/questions.py
+++ functions/questions.py
@@ -33,9 +33,10 @@ def getQuestions(userId, label):
     for i in range(0, len(res)):
         # Dans le dictionnaire, on a les valeurs de la question et
         # un tableau de réponses qui contient toutes les réponses associées à la question
+        print(res)
         dico = {
-            "id": res[i][0],
-            "enonce": res[i][1],
+            "id": res[i][1],
+            "enonce": res[i][0],
             "user": res[i][2],
             "etiquette": fonctionLabels.getLiensEtiquettes(res[i][0]),
             "reponses": getReponses(res[i][0])
