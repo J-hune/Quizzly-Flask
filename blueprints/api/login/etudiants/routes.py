9fab871a7c0dd200e9ad876fd43b562607f5edd4
Matéo <mateo.avventuriero@etu.umontpellier.fr>
2023-02-05 19:11:28 +0100

    [Func] Ajouts des nouveaux BluePrint enseignants/etudiants

---
+++ blueprints/api/login/etudiants/routes.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, jsonify, request, session
+from functions.login import userExists
+
+
+etudiants = Blueprint('etudiants', __name__, url_prefix='/etudiants')
+
+@etudiants.route('/signin', methods=['POST'])
+def signin():
+    data = request.get_json(force=True)
+    firstname = data.get("firstname")
+    surname = data.get("surname")
+    password = data.get("password")
+
+    # Si un des champs est vide (en plus de la vérification client)
+    if not (firstname and surname and password):
+        return jsonify({
+            "status": 401,
+            "reason": "First Name, Surname or Password Invalid"
+        }), 401
+    else:
+        # Si l'étudiant existe en base de donnée
+        if userExists(firstname, surname, password, "Etudiants"):
+            return jsonify(success=True), 200
+        else:
+            return jsonify({
+                "status": 401,
+                "reason": "First Name, Surname or Password Invalid"
+            }), 401
+
+
+@etudiants.route('/logged', methods=['GET'])
+def logged():
+    print(session)
+    if 'user' in session:
+        return jsonify(session["user"])
+    else:
+        return jsonify({
+            "status": 200,
+            "reason": "not Logged"
+        }), 200
--- blueprints/api/login/routes.py
+++ blueprints/api/login/routes.py
@@ -1,58 +1,8 @@
-from flask import Blueprint, jsonify, request, session
-from functions.login import registerUser, userExists
+from flask import Blueprint
+from blueprints.api.login.enseignants.routes import enseignants
+from blueprints.api.login.etudiants.routes import etudiants
 
 login = Blueprint('login', __name__, url_prefix='/login')
 
-
-@login.route('/signup', methods=['POST'])
-def signup():
-    data = request.get_json(force=True)
-    firstname = data.get("firstname")
-    surname = data.get("surname")
-    password = data.get("password")
-
-    # Si un des champs est vide (en plus de la vérification client)
-    if not (firstname and surname and password):
-        return jsonify({
-            "status": 401,
-            "reason": "First Name, Surname or Password Invalid"
-        }), 401
-    else:
-        registerUser(firstname, surname, password)
-        return jsonify(success=True), 200
-
-
-@login.route('/signin', methods=['POST'])
-def signin():
-    data = request.get_json(force=True)
-    firstname = data.get("firstname")
-    surname = data.get("surname")
-    password = data.get("password")
-
-    # Si un des champs est vide (en plus de la vérification client)
-    if not (firstname and surname and password):
-        return jsonify({
-            "status": 401,
-            "reason": "First Name, Surname or Password Invalid"
-        }), 401
-    else:
-        # Si l'utilisateur existe en base de donnée
-        if userExists(firstname, surname, password):
-            return jsonify(success=True), 200
-        else:
-            return jsonify({
-                "status": 401,
-                "reason": "First Name, Surname or Password Invalid"
-            }), 401
-
-
-@login.route('/logged', methods=['GET'])
-def logged():
-    print(session)
-    if 'user' in session:
-        return jsonify(session["user"])
-    else:
-        return jsonify({
-            "status": 200,
-            "reason": "not Logged"
-        }), 200
+login.register_blueprint(enseignants)
+login.register_blueprint(etudiants)
