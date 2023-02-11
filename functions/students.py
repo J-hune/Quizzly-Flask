51c62677ea44bd7e09eb5120a3ff2a470e9f64d9
Hugo <hgvaillant@gmail.com>
2023-02-11 15:03:39 +0100

    [Func] Ajout des fonctions de Livio (getEtu, addStudent, changePassword, removeStudent, removeAllStudent)

---
+++ functions/students.py
@@ -0,0 +1,122 @@
+import sqlite3
+
+
+# teste si l'étudiant existe dans la base de donnée
+def getEtu(studentId):
+    try:
+        # Connection à la table
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
+
+        # Selection des données dans la table
+        cur.execute("""SELECT nom, prenom FROM Etudiants WHERE id = ?;""", (studentId, ))
+        res = cur.fetchall()
+
+        # Fermeture de la connection
+        cur.close()
+        con.close()
+
+        if len(res) == 0:
+            return False
+        return True
+    except sqlite3.Error as error:
+        print("Échec de l'insertion de la variable Python dans la table sqlite", error)
+        return False
+
+
+# ajoute les étudiants dans la base de donnée
+# Reçois des données sous cette forme :
+# [{"numEtudiant" : 1332214, "nom":"DeLaTour", "prenom":"Jean"},
+# {"numEtudiant" : 1322324, "nom":"DeLaTour", "prenom":"Jeanne"},
+# ]
+def addStudent(ListeDeDictionnaire):
+    nbStudentAdd = 0
+    for i in range(len(ListeDeDictionnaire)):
+        try:
+            # Connection à la table
+            con = sqlite3.connect('database.db')
+            cur = con.cursor()
+
+            # Insertion des données dans la table
+            sql = """INSERT or IGNORE INTO Etudiants
+                    (id, nom, prenom, mdp) 
+                    VALUES (?, ?, ?, ?);"""
+            data = (ListeDeDictionnaire[i]["numEtudiant"], ListeDeDictionnaire[i]["nom"], ListeDeDictionnaire[i]["prenom"],ListeDeDictionnaire[i]["numEtudiant"])
+            cur.execute(sql, data)
+            con.commit()
+
+            # Si le dernier element inserer a le meme id que l'etudiant actuel on l'a réellement ajouté
+            if cur.lastrowid == ListeDeDictionnaire[i]["numEtudiant"]:
+                nbStudentAdd += 1
+
+            # Fermeture de la connection
+            cur.close()
+            con.close()
+        except sqlite3.Error as error:
+            print("Échec de l'insertion de la variable Python dans la table sqlite", error)
+            return -1
+    return nbStudentAdd
+
+
+def changePassword(NumEtudiant, password):
+    try:
+        # Connection à la table
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
+
+        # Mise à jour des données dans la table
+        cur.execute("""UPDATE Etudiants SET mdp = ? WHERE id = ?;""", (password ,NumEtudiant))
+        con.commit()
+
+        # Fermeture de la connection
+        cur.close()
+        con.close()
+        return True
+    except sqlite3.Error as error:
+        print("Échec de l'insertion de la variable Python dans la table sqlite", error)
+        return False
+
+
+# removeStudent renvoie :
+#  0 si bon
+#  1 si mauvaise requete
+#  2 si l'étudiant n'est pas trouvé
+def removeStudent(studentId):
+    # Si l'étudiant existe
+    if getEtu(studentId):
+        try:
+            # Connection à la table
+            con = sqlite3.connect('database.db')
+            cur = con.cursor()
+
+            # Suppression des données dans la table
+            cur.execute("""DELETE FROM Etudiants WHERE id = ?;""", (studentId ,))
+            con.commit()
+            # Fermeture de la connection
+            cur.close()
+            con.close()
+            return 0
+        except sqlite3.Error as error:
+            print("Échec de la supression de l'element dans la table sqlite", error)
+            return 1
+    else:
+        return 2
+
+
+def removeAllStudent():
+    try:
+        # Connection à la table
+        con = sqlite3.connect('database.db')
+        cur = con.cursor()
+
+        # Suppression de toutes les données dans la table
+        cur.execute("""DELETE FROM Etudiants;""")
+        con.commit()
+        print(cur.lastrowid)
+        # Fermeture de la connection
+        cur.close()
+        con.close()
+        return True
+    except sqlite3.Error as error:
+        print("Échec de la supression de l'element dans la table sqlite", error)
+        return False
