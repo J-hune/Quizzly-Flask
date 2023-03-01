86468582bfcbea3c73217f54d21260381d4ed171
Hugo <hgvaillant@gmail.com>
2023-03-01 14:27:29 +0100

    [Fix] quelques modifications pour les routes et les fonctions des sequences

--- blueprints/api/sequences/routes.py
+++ blueprints/api/sequences/routes.py
@@ -4,9 +4,10 @@ import functions.sequences as functionSequences
 
 sequences = Blueprint('sequences', __name__, url_prefix='/sequences')
 
+
 # Route pour insérer une Sequence
-@sequences.route('/AddSequence', methods=['POST'])
-def AddSequence():
+@sequences.route('/addSequence', methods=['POST'])
+def addSequence():
     # Si il est en session
     if 'user' in session:
 
@@ -14,7 +15,7 @@ def AddSequence():
         if session["user"]["type"] == "Enseignant":
             data = request.get_json(force=True)
             questions = data['questions']
-            if functionSequences.addSequence('user', questions):
+            if functionSequences.addSequence(session["user"]["id"], questions):
                 return jsonify({"success": True}), 200
             else:
                 return jsonify({
@@ -25,7 +26,7 @@ def AddSequence():
         else:
             return jsonify({
                 "status": 403,
-                "reason": "Forbidden"
+                "reason": "Permission non accordée"
             }), 403
     # Pas en session
     else:
@@ -41,32 +42,38 @@ def editSequence(id):
 
     # Si il est en session
     if 'user' in session:
-        if 'user' == functionSequences.GetEnseignant(id):
-            # Si c'est un enseignant
-            if session["user"]["type"] == "Enseignant":
+        # Si c'est un enseignant
+        if session["user"]["type"] == "Enseignant":
+            if session["user"]["id"] == functionSequences.getEnseignant(id):
                 data = request.get_json(force=True)
                 questions = data['questions']
                 # Renvoie 0 si bon
                 # 1 si mauvaise request
-                # 2 si l'étudiant n'est pas trouvé
+                # 2 si la sequence n'est pas trouvé
                 edit = functionSequences.editSequence(id, questions)
                 if edit == 0:
                     return jsonify(success=True), 200
                 elif edit == 2:
                     return jsonify({
                         "status": 400,
-                        "reason": "Sequence not found"
+                        "reason": "sequence introuvable"
                     }), 400
                 elif edit == 1:
                     return jsonify({
                         "status": 400,
                         "reason": "Edit sequence Invalid"
                     }), 400
+            # Ce n'est pas sa sequence
+            else:
+                return jsonify({
+                    "status": 400,
+                    "reason": "Ce n'est pas votre sequence, ou sequence introuvable"
+                }), 400
         # Ce n'est pas un enseignant
         else:
             return jsonify({
                 "status": 403,
-                "reason": "Forbidden"
+                "reason": "Permission non accordée"
             }), 403
     # Pas en session
     else:
@@ -75,17 +82,19 @@ def editSequence(id):
             "reason": "Session non disponible"
         }), 400
 
+
 @sequences.route('/removeSequence/<id>', methods=['GET'])
 def removeSequence(id):
     # Si il est en session
     if 'user' in session:
-        if 'user' == functionSequences.GetEnseignant(id):
-            # Si c'est un enseignant
-            if session["user"]["type"] == "Enseignant":
+        # Si c'est un enseignant
+        if session["user"]["type"] == "Enseignant":
+            print(functionSequences.getEnseignant(id))
+            if session["user"]["id"] == functionSequences.getEnseignant(id):
                 # Renvoie 0 si bon
                 # 1 si mauvaise request
                 # 2 si l'étudiant n'est pas trouvé
-                remove = functionSequences.RemoveSequence(id)
+                remove = functionSequences.removeSequence(id)
                 if remove == 0:
                     return jsonify(success=True), 200
                 elif remove == 2:
@@ -98,12 +107,17 @@ def removeSequence(id):
                         "status": 400,
                         "reason": "Edit sequence Invalid"
                     }), 400
-
+            # Ce n'est pas sa sequence
+            else:
+                return jsonify({
+                    "status": 400,
+                    "reason": "Ce n'est pas votre sequence, ou sequence introuvable"
+                }), 400
         # Ce n'est pas un enseignant
         else:
             return jsonify({
                 "status": 403,
-                "reason": "Forbidden"
+                "reason": "Permission non accordée"
             }), 403
     # Pas en session
     else:
--- functions/sequences.py
+++ functions/sequences.py
@@ -1,20 +1,26 @@
 import sqlite3
 
-def GetEnseignant(idSequence):
+
+def getEnseignant(idSequence):
     try:
         # Connection à la table
         con = sqlite3.connect('database.db')
         cur = con.cursor()
 
         # Selection des données dans la table
-        cur.execute("SELECT enseignant FROM Sequences WHERE id = ?;", (idSequence,))
+        result = cur.execute("SELECT enseignant FROM Sequences WHERE id = ?;", (idSequence,))
+
+        result = result.fetchone()
         # Fermeture de la connection
         cur.close()
         con.close()
-
+        if result:
+            return result[0]
+        return False
     except sqlite3.Error as error:
         print("Échec de l'insertion de la variable Python dans la table sqlite", error)
         return False
+
 
 def addSequence(enseignantId, TabIdQuestions):
     try:
@@ -23,7 +29,7 @@ def addSequence(enseignantId, TabIdQuestions):
         cur = con.cursor()
 
         # insertion des données dans la table
-        cur.execute("INSERT INTO Sequences (enseignant) VALUES (?);", enseignantId)
+        cur.execute("INSERT INTO Sequences (enseignant) VALUES (?);", (enseignantId, ))
 
         # Récupère la clé primaire de cette sequence
         SequencesId=cur.lastrowid
@@ -33,20 +39,21 @@ def addSequence(enseignantId, TabIdQuestions):
 
             # insertion des données dans la table
             cur.execute("INSERT or IGNORE INTO liensSequencesQuestions (idSequence, idQuestion) VALUES (?,?);", (SequencesId, TabIdQuestions[i]))
-        con.commit()
+            con.commit()
 
         # Fermeture de la connection
         cur.close()
         con.close()
-
+        return True
     except sqlite3.Error as error:
         print("Échec de l'insertion de la variable Python dans la table sqlite", error)
         return False
 
+
 def editSequence(SequenceId, TabIdQuestions):
 
     # Si la sequence existe
-    if GetEnseignant(SequenceId):
+    if getEnseignant(SequenceId):
         try:
             # Connection à la table
             con = sqlite3.connect('database.db')
@@ -61,7 +68,7 @@ def editSequence(SequenceId, TabIdQuestions):
                 # insertion des données dans la table
                 cur.execute("INSERT or IGNORE INTO liensSequencesQuestions (idSequence, idQuestion) VALUES (?,?);",
                             (SequenceId, TabIdQuestions[i]))
-            con.commit()
+                con.commit()
 
             # Fermeture de la connection
             cur.close()
@@ -76,10 +83,11 @@ def editSequence(SequenceId, TabIdQuestions):
     #  1 si mauvaise request
     #  2 si la sequence n'est pas trouvé
 
-def RemoveSequence(SequenceId):
+
+def removeSequence(SequenceId):
 
     # Si la sequence existe
-    if GetEnseignant(SequenceId):
+    if getEnseignant(SequenceId):
         try:
             # Connection à la table
             con = sqlite3.connect('database.db')
