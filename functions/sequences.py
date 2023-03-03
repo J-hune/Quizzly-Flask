899d226b206d334d0b18952cb9b5e817a4837710
Matéo <mateo.avventuriero@etu.umontpellier.fr>
2023-03-03 05:13:03 +0100

    [Edit] Correction id séquence qui s'auto incrémente

--- functions/sequences.py
+++ functions/sequences.py
@@ -3,16 +3,16 @@ import sqlite3
 from functions.broadcast import generateCode
 
 
-def getEnseignant(idSequence):
+def getEnseignant(sequenceId):
     try:
         # Connection à la table
         con = sqlite3.connect('database.db')
         cur = con.cursor()
 
         # Selection des données dans la table
-        result = cur.execute("SELECT enseignant FROM Sequences WHERE id = ?;", (idSequence,))
-
+        result = cur.execute("SELECT enseignant FROM Sequences WHERE id = ?;", (sequenceId,))
         result = result.fetchone()
+
         # Fermeture de la connection
         cur.close()
         con.close()
@@ -24,24 +24,24 @@ def getEnseignant(idSequence):
         return False
 
 
-def addSequence(enseignantId, titreSequence, TabIdQuestions):
+def addSequence(enseignant, titre, tabIdQuestions):
     try:
         # Connection à la table
         con = sqlite3.connect('database.db')
         cur = con.cursor()
 
-        # Génération de l'id de la séquence
-        code = generateCode()
-
         # insertion des données dans la table
-        cur.execute("INSERT INTO Sequences (id, titre, enseignant) VALUES (?, ?, ?);",
-                    (code, titreSequence, enseignantId,))
+        cur.execute("INSERT INTO Sequences (titre, enseignant) VALUES (?, ?);",
+                    (titre, enseignant,))
+
+        # Récupère la valeur du dernier auto-increment
+        lastId = cur.lastrowid
 
         # Pour chaque question
-        for i in range(len(TabIdQuestions)):
+        for i in range(len(tabIdQuestions)):
             # insertion des données dans la table
-            cur.execute("INSERT or IGNORE INTO liensSequencesQuestions (idSequence, idQuestion) VALUES (?,?);",
-                        (code, TabIdQuestions[i]))
+            cur.execute("INSERT or IGNORE INTO liensSequencesQuestions (idSequence, idQuestion) VALUES (?, ?);",
+                        (lastId, tabIdQuestions[i]))
             con.commit()
 
         # Fermeture de la connection
@@ -53,26 +53,26 @@ def addSequence(enseignantId, titreSequence, TabIdQuestions):
         return False
 
 
-def editSequence(SequenceId, titre, TabIdQuestions):
-    # Si la sequence existe
-    if getEnseignant(SequenceId):
+def editSequence(sequenceId, titre, tabIdQuestions):
+    # Si la séquence existe
+    if getEnseignant(sequenceId):
         try:
             # Connection à la table
             con = sqlite3.connect('database.db')
             cur = con.cursor()
 
             # Modification du titre de la séquence
-            cur.execute("UPDATE Sequences SET titre = ? WHERE id = ?", (titre, SequenceId))
+            cur.execute("UPDATE Sequences SET titre = ? WHERE id = ?", (titre, sequenceId))
 
             # Suppression des données dans la table
-            cur.execute("DELETE FROM liensSequencesQuestions WHERE idSequence = ?;", (SequenceId,))
+            cur.execute("DELETE FROM liensSequencesQuestions WHERE idSequence = ?;", (sequenceId,))
             con.commit()
 
             # pour chaque question
-            for i in range(len(TabIdQuestions)):
+            for i in range(len(tabIdQuestions)):
                 # insertion des données dans la table
                 cur.execute("INSERT or IGNORE INTO liensSequencesQuestions (idSequence, idQuestion) VALUES (?,?);",
-                            (SequenceId, TabIdQuestions[i]))
+                            (sequenceId, tabIdQuestions[i]))
                 con.commit()
 
             # Fermeture de la connection
@@ -90,18 +90,16 @@ def editSequence(SequenceId, titre, TabIdQuestions):
     # 2 si la sequence n'est pas trouvée
 
 
-def removeSequence(SequenceId):
-    # Si la sequence existe
-    if getEnseignant(SequenceId):
+def removeSequence(sequenceId):
+    # Si la séquence existe
+    if getEnseignant(sequenceId):
         try:
             # Connection à la table
             con = sqlite3.connect('database.db')
             cur = con.cursor()
 
-            # Suppression des données dans la table
-            cur.execute("DELETE FROM liensSequencesQuestions WHERE idSequence = ?;", (SequenceId,))
-            con.commit()
-            cur.execute("DELETE FROM Sequences WHERE id = ?;", (SequenceId,))
+            # Suppression des données dans la table (update on cascade supprime les liens)
+            cur.execute("DELETE FROM Sequences WHERE id = ?;", (sequenceId,))
             con.commit()
 
             # Fermeture de la connection
