1b9f508a3d644b9805739fd06d85f3b33d619862
Matéo <mateo.avventuriero@etu.umontpellier.fr>
2023-03-12 01:52:09 +0100

    [Edit] Correction de /api/route.py (suppression des liens avec user)

--- blueprints/api/routes.py
+++ blueprints/api/routes.py
@@ -4,14 +4,12 @@ from blueprints.api.students.routes import students
 from blueprints.api.labels.routes import label
 from blueprints.api.login.routes import login
 from blueprints.api.questions.routes import questions
-from blueprints.api.users.routes import users
 from blueprints.api.sequences.routes import sequences
 
 api = Blueprint('api', __name__, url_prefix='/api')
 api.register_blueprint(label)
 api.register_blueprint(login)
 api.register_blueprint(questions)
-api.register_blueprint(users)
 api.register_blueprint(students)
 api.register_blueprint(sequences)
 
