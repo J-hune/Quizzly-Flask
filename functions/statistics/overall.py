2d8cc6c79b0082afd8b356679305e7bdcd7d485a
Matéo <mateo.avventuriero@etu.umontpellier.fr>
2023-03-18 13:33:23 +0100

    [Edit] amélioration de getOverallStats
    Deux requêtes au lieu de beaucoup plus
    Correction getArchives qui ne renvoyait pas les diffusions vides

--- functions/statistics/overall.py
+++ functions/statistics/overall.py
@@ -2,165 +2,148 @@ import sqlite3
 from datetime import datetime, time
 
 
-# Compte le nombre de diffusions et le nombre de questions posées d'un enseignant
-# Param : - cursor : cursor de la BDD
-#         - enseignant : l'id de l'enseignant (int)
-# Return : le nombre de diffusions et le nombre de questions posées (tuple d'int)
-def countTotalQuiz(cursor, enseignant):
-    try:
-        # Compte le nombre diffusion d'un enseignant selon le mode
-        #                         nbQuiz                 nbQuestion
-        cursor.execute("SELECT COUNT(DISTINCT AD.id), COUNT(DISTINCT AQ.id) \
-                            FROM ArchivesDiffusions AD \
-                            JOIN ArchivesQuestions AQ \
-                            WHERE enseignant = ?;", (enseignant,))
-        result = cursor.fetchone()
-        return result
-
-    except sqlite3.Error as error:
-        print("Une erreur est survenue lors de la sélection du nombre de diffusion :", error)
-        return 0
-
-
-# Calcule le pourcentage de réussite total (moyenne) parmi toutes les diffusions d'un enseignant
+# Récupère la moyenne du taux de réussite et la moyenne du nombre de participants pour chaque nb_jour derniers jours
+# Pour le nombre participants, on affiche tous les jours même quand il n'y a pas eu de quiz diffusé
+# Pour le taux de réussite, on affiche uniquement les jours où un quiz a été diffusé
 # Param : - cursor : cursor de la BDD
 #         - enseignant : id de l'enseignant (int)
-# Return : le pourcentage de réussite totale (float)
-def countTotalPourcentage(cursor, enseignant):
+#         - nb_jour : nombre de jours à calculer (int)
+# Return : - participation : le nombre de participants au cours du temps (dico de tab)
+#                   {     (-> nombre de participant à des questions et séquence pour chaque jour)
+#                    "days": [1646937600, 1647024000, 1647110400, 1647196800, 1647283200],
+#                    "sequences": [10, 20, 0, 40, 50],
+#                    "questions": [2, 0, 4, 50, 0]
+#                   }
+#           - success : les pourcentages de réussite au cours du temps (dico de tab)
+#                   {           (-> taux de réussite pour chaque quiz/diffusion)
+#                    "days": [1646937600, 1647024000, 1647110400, 1647196800, 1647283200],
+#                    "quiz":[10, 25, 7, 19, 13, 55, 28]
+#                   }
+
+#           /!\ chaque index des tableaux correspondent à la même donnée (pour le jour jours[i],
+#               participantsSequences[i] personnes ont participé à des séquences et participantsQuestions[i]
+#               personnes ont participé à des questions)
+def getTemporalStats(cursor, enseignant, nb_jour):
     try:
-        # Compte le nombre de réponses et le nombre de bonnes réponses
-        #                        nbRéponse      nbBonneRéponse
-        cursor.execute("SELECT COUNT(AR.id), SUM(AR.est_correcte) \
-                        FROM ArchivesDiffusions AD \
-                        JOIN ArchivesQuestions AQ ON AD.id = AQ.diffusion \
-                        JOIN ArchivesReponses AR ON AQ.id = AR.question \
-                        WHERE enseignant = ? \
-                        GROUP BY AD.id;", (enseignant,))
-        result = cursor.fetchall()
+        jour = datetime.timestamp(datetime.combine(datetime.now(), time.min))  # Date du jour actuel (à minuit pile)
+        jour -= (86400 * (nb_jour-1))  # Jour il y a nb_jour
 
-        nb_quiz = len(result)
-        pourcentage = 0
-        # Si au moins une diffusion (sinon le pourcentage total reste à 0%)
-        if nb_quiz > 0:
-            # Pour chaque diffusion
-            for i in range(nb_quiz):
-                pourcentage += result[i][1] / result[i][0]  # Additionne le pourcentage de toutes les diffusions
-
-            pourcentage = (pourcentage / nb_quiz) * 100  # Calcule la moyenne de toutes les diffusions
-
-        return pourcentage
-
-    except sqlite3.Error as error:
-        print("Une erreur est survenue lors de la sélection du pourcentage :", error)
-        return 0
-
-
-# Compte le nombre de participants à une diffusion pour un jour et un mode donné
-# Param : - cursor : cursor BDD
-#         - jour : la date du jour en timestamp (int)
-#         - mode_diffusion : le mode de diffusion choisi, 0 pour une question, 1 pour une séquence (int)
-# Return : le nombre de participants à une diffusion de question (si plus d'une diffusion, moyenne de ces diffusions)
-def countParticipantByDay(cursor, jour, mode_diffusion, enseignant):
-    try:
-        # Compte le nombre de participants pour chaque diffusion du jour (ne compte pas les diffusions vides)
-        cursor.execute("SELECT COUNT(DISTINCT AR.etudiant) \
-                        FROM ArchivesDiffusions AD \
-                        JOIN ArchivesQuestions AQ ON AD.id = AQ.diffusion \
-                        JOIN ArchivesReponses AR ON AQ.id = AR.question \
-                        WHERE AD.date >= ? AND AD.date <= ? \
-                        AND AD.mode = ? \
-                        AND AD.enseignant = ? \
-                        GROUP BY AD.id;",
-                       (jour, jour + 86399.999999, mode_diffusion, enseignant))  # le début du jour (00:00) et la fin du jour (23:59)
-        result = cursor.fetchall()
-
-        nb_quiz = len(result)
-        nb_participant = 0
-        if nb_quiz > 0:
-            # Additionne les participants de toutes les diffusions du jour
-            for i in range(nb_quiz):
-                nb_participant += result[i][0]
-
-            # Calcule la moyenne des participants du jour (nb participant total / nb diffusion)
-            nb_participant = nb_participant / nb_quiz
-
-        return nb_participant
-
-    except sqlite3.Error as error:
-        print("Une erreur est survenue lors de la sélection du nombre de participants :", error)
-        return 0
-
-
-# Récupère le taux de réussite pour chaque quiz
-# Param : - cursor : cursor de la BDD
-#         - enseignant : id de l'enseignant (int)
-# Return : un dico de tableau avec le taux de réussite
-def getSuccessByDay(cursor, jour, enseignant):
-    try:
-        # Compte le nombre de réponses et le nombre de bonnes réponses
-        #                        nbRéponse      nbBonneRéponse
-        cursor.execute("SELECT COUNT(AR.id), SUM(AR.est_correcte) \
+        # Récupère la date, le mode, le nombre de participants, le taux de réussite pour tous les quiz après le jour
+        #                                           nbParticipant              nbRéponse      nbBonneRéponse
+        cursor.execute("SELECT AD.date, AD.mode, COUNT(DISTINCT AR.etudiant), COUNT(AR.id), SUM(AR.est_correcte) \
                                 FROM ArchivesDiffusions AD \
                                 JOIN ArchivesQuestions AQ ON AD.id = AQ.diffusion \
                                 JOIN ArchivesReponses AR ON AQ.id = AR.question \
-                                WHERE AD.date >= ? AND AD.date <= ? \
+                                WHERE AD.date >= ? \
                                 AND AD.enseignant = ? \
-                                GROUP BY AD.id;", (jour, jour + 86399.999999, enseignant))
+                                GROUP BY AD.id \
+                                ORDER BY AD.date;", (jour, enseignant))
         result = cursor.fetchall()
 
-        nb_quiz = len(result)
-        taux_reussite = 0
-        # Si au moins une diffusion (sinon taux de réussite reste à 0)
-        if nb_quiz > 0:
-            # Additionne les taux de réussite de toutes les diffusions du jour
-            for i in range(nb_quiz):
-                taux_reussite += result[i][1]/result[i][0]
+        nb_quiz = len(result)  # Le nombre de quiz diffusés durant les nb_jour derniers jours
+        participation = {"days": [], "sequences": [], "questions": []}
+        success = {"days": [], "quiz": []}
 
-            taux_reussite = (taux_reussite * 100) / nb_quiz  # Calcule la moyenne du taux de réussite du jour
+        k = 0
+        # Pour les nb_jour derniers jours
+        for i in range(nb_jour):
 
-        return taux_reussite
+            nb_quiz_jour = 0  # Nombre de quiz ce jour
+            nb_participant_sequence = 0  # Nombre de participants à des séquences ce jour
+            nb_participant_question = 0  # Nombre de participants à des questions ce jour
+            success_rate = 0  # Nombre de quiz ce jour
+
+            # Pour tous les quiz du jour
+            while k < nb_quiz and result[k][0] < jour + 86400:
+                nb_quiz_jour += 1
+                success_rate += result[k][4] / result[k][3]
+                # Si le quiz est une question
+                if result[k][1] == 0:
+                    nb_participant_question += result[k][2]
+                # Sinon c'est une séquence
+                else:
+                    nb_participant_sequence += result[k][2]
+                k += 1
+
+            participation["days"].append(jour)
+            # S'il y a eu un quiz ce jour-là, on fait une moyenne des stats
+            if nb_quiz_jour > 0:
+                success["days"].append(jour)
+                success["quiz"].append((success_rate / nb_quiz_jour) * 100)
+                participation["sequences"].append(nb_participant_sequence / nb_quiz_jour)
+                participation["questions"].append(nb_participant_question / nb_quiz_jour)
+            # Sinon, on met 0 pour les participants (et on n'enregistre pas le taux de succès du jour)
+            else:
+                participation["sequences"].append(0)
+                participation["questions"].append(0)
+
+            jour += 86400  # Jour suivant
+
+        return participation, success
 
     except sqlite3.Error as error:
-        print("Une erreur est survenue lors de la sélection du taux de réussite des quiz :", error)
+        print("Une erreur est survenue lors de la sélection des statistiques temporelles :", error)
         return 0
 
 
-# Récupère toutes les archives des anciennes diffusions (quiz) d'un enseignant
+# Récupère toutes les archives des anciennes diffusions et ordonnes les données en statistiques
 # Param : - cursor : cursor de la BDD
 #         - enseignant : id de l'enseignant (int)
-# Return : un tableau de dico avec les archives des quiz
-#           [
-#            { "archiveId": 1, "title": "Séquence algorithmie", "id": "Fxa4t3xr", "date": 1678667302, "participantCount": 15, "percentCorrect": 42 },
-#            { "archiveId": 2, "title": "Les questions de sciences", "id": "Gxa4t3xr", "date": 1678667402, "participantCount": 15, "percentCorrect": 32},
-#            {...}, ...
-#           ]
+# Return : - nb_quiz : le nombre de diffusions total effectuées (int)
+#          - nb_question_total : le nombre de questions total posées (int)
+#          - taux_reussite_total : le taux de réussite total en pourcentage (float)
+#          - archives : les stats de toutes les diffusions effectuées (tab de dico)
+#               [
+#                { "archiveId": 1, "title": "Séquence algorithmie", "id": "Fxa4t3xr", "date": 1678667302, "participantCount": 15, "percentCorrect": 42 },
+#                { "archiveId": 2, "title": "Les questions de sciences", "id": "Gxa4t3xr", "date": 1678667402, "participantCount": 15, "percentCorrect": 32},
+#                {...}, ...
+#               ]
 def getArchives(cursor, enseignant):
     try:
-        # Récupère l'id du quizz, son titre, son code, sa date, son mode, le nombre de participant et le pourcentage de bonne réponse
-        #                                                                       nbParticipant             nbRéponse        nbBonneRéponse
-        cursor.execute("SELECT AD.id, AD.titre, AD.code, AD.date, AD.mode, COUNT(DISTINCT AR.etudiant), COUNT(AR.id), SUM(AR.est_correcte) \
+        # L'id, le titre, le code, la date, le mode, le nombre de participants, le taux de réussite de chaque quiz
+        #                                                                       nbParticipant             nbRéponse     #nbBonneRéponse       nbQuestion
+        cursor.execute("SELECT AD.id, AD.titre, AD.code, AD.date, AD.mode, COUNT(DISTINCT AR.etudiant), COUNT(AR.id), SUM(AR.est_correcte), COUNT(DISTINCT AQ.id) \
                         FROM ArchivesDiffusions AD \
-                        JOIN ArchivesQuestions AQ ON AD.id = AQ.diffusion \
-                        JOIN ArchivesReponses AR ON AQ.id = AR.question \
-                        WHERE enseignant = ? \
+                        LEFT JOIN ArchivesQuestions AQ ON AD.id = AQ.diffusion \
+                        LEFT JOIN ArchivesReponses AR ON AQ.id = AR.question \
+                        WHERE AD.enseignant = ? \
                         GROUP BY AD.id \
-                        ORDER BY AD.date DESC;",(enseignant, ))
+                        ORDER BY AD.date DESC;", (enseignant, ))
         result = cursor.fetchall()
 
+        nb_quiz_done = 0  # Le nombre de diffusions réellement effectué
+        nb_quiz = len(result)  # Le nombre de diffusions total
+        nb_question_total = 0  # Le nombre de questions posées
+        taux_reussite_total = 0  # Le pourcentage de réussite total
+        archives = []  # Les archives des diffusions
+
         # Ordonne les données dans un tableau de dico pour chaque quiz
-        archives = []
-        for i in range(len(result)):
+        for i in range(nb_quiz):
             data = {"archiveId": result[i][0],
                     "title": result[i][1],
                     "id": result[i][2],
                     "date": result[i][3],
                     "mode": result[i][4],
-                    "participantCount": result[i][5],
-                    "percentCorrect": (result[i][7] / result[i][6]) * 100
+                    "participantCount": result[i][5]
                     }
+
+            nb_question_total += result[i][8]  # Additionne les questions posées
+
+            # Si le quiz a réellement été effectué (ce n'est pas une diffusion vide)
+            if result[i][7] is not None:
+                pourcentage = (result[i][7] / result[i][6]) * 100  # Calcule le pourcentage du quiz
+                data["percentCorrect"] = pourcentage
+                taux_reussite_total += pourcentage
+                nb_quiz_done += 1
+            # Sinon, on met le pourcentage à 0, et on ne le compte pas dans le pourcentage total
+            else:
+                data["percentCorrect"] = 0
+
             archives.append(data)
 
-        return archives
+        taux_reussite_total = taux_reussite_total / nb_quiz_done
+
+        return nb_quiz, nb_question_total, taux_reussite_total, archives
 
     except sqlite3.Error as error:
         print("Une erreur est survenue lors de la sélection des archives des quiz :", error)
@@ -203,42 +186,18 @@ def getOverallStats(enseignant, nb_jour):
         # Active les clés étrangères
         cursor.execute("PRAGMA foreign_keys = ON")
 
-        # Compte le nombre total de diffusions de question et de quiz (diffusion), puis ordonne les données
-        total = countTotalQuiz(cursor, enseignant)
+        temporal_stats = getTemporalStats(cursor, enseignant, nb_jour)
+        globalstats_and_archives = getArchives(cursor, enseignant)
+
+        # Ordonne les données dans un dico
         data = {
-            "totalQuizzes": total[0],
-            "totalQuestions": total[1],
-            "successRate": countTotalPourcentage(cursor, enseignant),
-            "participation": {"days": [], "sequences": [], "questions": []},
-            "success": {"days": [], "quiz": []},
-            "archives": getArchives(cursor, enseignant)
+            "totalQuizzes": globalstats_and_archives[0],
+            "totalQuestions": globalstats_and_archives[1],
+            "successRate": globalstats_and_archives[2],
+            "participation": temporal_stats[0],
+            "success": temporal_stats[1],
+            "archives": globalstats_and_archives[3]
         }
-
-        # Date du jour actuel (à minuit pile)
-        jour = datetime.timestamp(datetime.combine(datetime.now(), time.min))
-
-        # Pour tous les "nb_jour" dernier jour, on calcule le taux de réussite et le nombre de participants
-        for i in range(nb_jour):
-
-            data["participation"]["days"].append(jour)
-
-            success = getSuccessByDay(cursor, jour, enseignant)
-            # S'il y a au moins une diffusion ce jour-là
-            if success:
-                # On récupère la moyenne du taux de succès du jour
-                data["success"]["days"].append(jour)
-                data["success"]["quiz"].append(success)
-
-                # On récupère la moyenne du nombre de participants du jour
-                data["participation"]["questions"].append(countParticipantByDay(cursor, jour, 0, enseignant))
-                data["participation"]["sequences"].append(countParticipantByDay(cursor, jour, 1, enseignant))
-
-            # Sinon, on ajoute directement 0 pour la participation (on n'ajoute pas le taux de réussite si aucun quiz)
-            else:
-                data["participation"]["questions"].append(0)
-                data["participation"]["sequences"].append(0)
-
-            jour -= 86400  # Jour précédent
 
         # Fermeture de la connection
         cursor.close()
@@ -249,3 +208,4 @@ def getOverallStats(enseignant, nb_jour):
     except sqlite3.Error as error:
         print("Une erreur est survenue lors de la sélection des statistiques :", error)
         return False
+
